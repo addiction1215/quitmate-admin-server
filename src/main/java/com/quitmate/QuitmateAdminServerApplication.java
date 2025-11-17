8a42ee0563c44b2f7d8d2022ab2c27f5235c676c
TNM-SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2025-11-17 17:57:14 +0900

    feat: start project

---
+++ src/main/java/com/quitmate/QuitmateAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.quitmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuitmateAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuitmateAdminServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quitmate/QuitmateAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quitmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuitmateAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
